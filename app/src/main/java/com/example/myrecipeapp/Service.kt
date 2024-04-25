10b06b22291579162d6b5bb754d02060305a748f
Kecheng Tao <tkc1364411045@gmail.com>
2024-04-25 14:05:41 -0400

    create recipe app

---
+++ app/src/main/java/com/example/myrecipeapp/Service.kt
@@ -0,0 +1,19 @@
+package com.example.myrecipeapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+private val retrofit = Retrofit.Builder().baseUrl("https://www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+val recipeService = retrofit.create(Service::class.java)
+
+interface Service {
+
+    @GET("categories.php")
+    suspend fun getCategories(): CategoriesResponse
+
+
+}
